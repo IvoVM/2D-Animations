366447311af741cf9b05c0440ad6ade781086dc9
IvoVM <ivomastrangelo12@gmail.com>
2023-11-28 12:24:17 -0300

    Created components bottomsheet and bottomSheet menu

---
+++ src/BottomSheet/Menu.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { Home } from "../YouTube/Home";
+import {
+  AnimationScreenNames,
+  allScreens,
+} from "../YouTube/NavigationHelpers/NavigationHelpers";
+
+const Stack = createStackNavigator();
+
+const getScreenConfig = (screen: string) => {
+  switch (screen) {
+    case AnimationScreenNames.WAVE_METER:
+    case AnimationScreenNames.BENDING_CIRCLE:
+      return {
+        headerTintColor: "#fff",
+        headerStyle: {
+          backgroundColor: "black",
+        },
+      };
+    case AnimationScreenNames.GRADIENT_CLOCK:
+      return {
+        headerShown: false,
+      };
+    default:
+      return undefined;
+  }
+};
+
+export const BottomSheetMenu = () => {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="YouTube Demos">
+        <Stack.Screen name="YouTube Demos" component={Home} />
+        {allScreens.map((screen: any, index: any) => (
+          <Stack.Screen
+            key={index}
+            name={screen.name}
+            component={screen.component}
+            options={getScreenConfig(screen.name)}
+          />
+        ))}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
